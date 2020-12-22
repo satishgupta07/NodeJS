80bb150e3c529882cdfe8123fc83865ea1783439
unknown <satishgupta.cse01@gmail.com>
2020-12-22 15:37:10 +0530

    GET Request
    Signed-off-by: unknown <satishgupta.cse01@gmail.com>

--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -2,7 +2,15 @@ const express = require('express')
 const server = express()
 
 server.get('/',(req,res)=> {
-    res.send('Hello World!')
+    res.send(`Hello Everyone !!`)
+})
+
+server.get('/hello/:name',(req,res)=> {
+    res.send(`Hello World ! ${req.params.name}`)
+})
+
+server.get('/bye',(req,res)=> {
+    res.send(`Good Bye ! ${req.query.name} ${req.query.age}`)
 })
  
 server.listen(4444,()=> {
