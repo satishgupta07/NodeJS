ad251a7f7e36c879580eab278910d0223da5a8b6
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-24 07:47:52 +0530

    Created Tables
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- src/models/db.js
+++ src/models/db.js
@@ -9,6 +9,40 @@ const db = new Sequelize(
     }
 )
 
+const User = db.define('user', {
+    username : {type: DataTypes.STRING(30), unique: true, allowNull: false},
+    password: {type: DataTypes.STRING, allowNull: true }
+})
+
+const Article = db.define('article', {
+    title: {
+        type: DataTypes.STRING(150),
+        allowNull: true
+    },
+    content: {
+        type : DataTypes.STRING
+    }
+})
+
+const Comment = db.define('comment', {
+    title: {
+        type: DataTypes.STRING(150),
+        allowNull: true
+    },
+    message: {
+        type : DataTypes.STRING
+    }
+})
+
+Article.belongsTo(User, {as: 'author'})
+User.hasMany(Article, {as: 'author'})
+
+Comment.belongsTo(Article)
+Article.hasMany(Comment)
+
+Comment.belongsTo(User)
+User.hasMany(Comment)
+
 module.exports = {
     db
 }
