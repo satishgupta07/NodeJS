aca18088619e0fe17526a3ae23bc38c401810e5d
unknown <satishgupta.cse01@gmail.com>
2021-01-01 12:36:06 +0530

    MongoDB Operators
    Signed-off-by: unknown <satishgupta.cse01@gmail.com>

--- Learning-Mongo/read.js
+++ Learning-Mongo/read.js
@@ -1,25 +1,43 @@
 const {MongoClient} = require('mongodb')
 const DB_URL = 'mongodb://localhost:27017'
 
-MongoClient.connect(DB_URL,async (err,client)=>{
+MongoClient.connect(DB_URL, (err,client)=>{
     if(err) throw err
     
-    const testdb = await client.db('testdb')
+    const testdb = client.db('testdb')
     
-    const people = await testdb.collection('people')
+    const people = testdb.collection('people')
 
-    // await people.find({}).toArray((err, result) => {
+    // people.find({}).toArray((err, result) => {
     //     if(err) throw err
     //     console.log(result)
     // })
 
-    let cursor = await people.find({})
-    let counter = 0
-    // cursor.skip(3)
-    while(await cursor.hasNext() && counter<4){
-        console.log(await cursor.next())
-        counter++
-    }
+    // let cursor = await people.find({})
+    // let counter = 0
+    // // cursor.skip(3)
+    // while(await cursor.hasNext() && counter<4){
+    //     console.log(await cursor.next())
+    //     counter++
+    // }
+
+    people.find({
+        $and :[
+            {
+                age:{
+                    // $eq:21
+                    $lt:20
+                }
+            },
+            {
+                city:'Delhi'
+            }
+        ]
+        })
+          .toArray((err,result)=>{
+        if(err) throw err
+        console.log(result)
+    })
 
     client.close()
 
